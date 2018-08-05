ebea044bc6caac4c88cb04074490894246a26cb1
Chu Song <chuchusong23@gmail.com>
2018-08-05 10:23:14 -0700

    Frontend deploy.

--- src/components/signinform/SignInFormContainer.js
+++ src/components/signinform/SignInFormContainer.js
@@ -3,19 +3,12 @@ import * as userAction from "../../actions/userAction";
 import SignInForm from "./SignInForm";
 
 export const mapStateToProps = state => {
-    /**
-     * state is redux global store
-     */
     return {
         user: state.currentUserReducer.user
     };
 };
 
 export const mapDispatchToProps = dispatch => {
-    /**
-     * dispatch refers to functions from actions
-     * bind with reducer
-     */
     return {
         signIn: (params) => {
             return dispatch(userAction.signIn(params));
